2980e9ac940b83c7e0436d0795182a7df1e780a0
HyunJin Lee <hjleesm@naver.com>
2018-09-03 23:29:13 +0900

    Added weekly count

--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter} from '@angular/core';
+import { Injectable} from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Http } from '@angular/http';
 
@@ -8,11 +8,14 @@ const API_URL = environment.apiUrl;
   providedIn: 'root'
 })
 export class AnalyticsService {
-  onCount: EventEmitter<any> = new EventEmitter();
 
   constructor(private http: Http) { }
 
   getCount() {
     return this.http.get(API_URL + '/api/lotto/count');
+  }
+
+  getWeeklyCount() {
+    return this.http.get(API_URL + '/api/lotto/count/weekly');
   }
 }
--- src/app/analytics/analytics/analytics.component.ts
+++ src/app/analytics/analytics/analytics.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from '../analytics.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-analytics',
@@ -8,12 +9,44 @@ import { AnalyticsService } from '../analytics.service';
 })
 export class AnalyticsComponent implements OnInit {
   count = 0;
+  chart = [];
 
   constructor(private analyticsService: AnalyticsService) { }
 
   ngOnInit() {
     this.analyticsService.getCount().subscribe(data => {
       this.count = data.json();
+    });
+
+    this.analyticsService.getWeeklyCount().subscribe(data => {
+      var countData = data.json();
+      var label = [];
+
+      countData.date = countData.date.reverse();
+      for(var i = 0; i < countData.date.length - 1; i++) {
+        var dt = new Date(countData.date[i]);
+        label.push(dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate());
+      }
+      label.push('이번 주');
+
+      this.chart = new Chart('canvas', 
+      {
+        type: 'bar',
+        data: {
+          labels: label,
+          datasets: [{
+            label: '번호 셋 개수',
+            data: countData.counts.reverse(),
+            borderWidth: 1
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: '주 별 생성된 번호 셋 개수'
+          }
+        }
+      });
     });
   }
 
