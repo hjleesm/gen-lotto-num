89285a3f01be3b00e6e5928aa6e75688cafb0ff2
HyunJin Lee <hjleesm@naver.com>
2018-09-17 23:17:49 +0900

    Added month chart

--- src/app/analytics/analytics/analytics.component.ts
+++ src/app/analytics/analytics/analytics.component.ts
@@ -10,6 +10,7 @@ import { Chart } from 'chart.js';
 export class AnalyticsComponent implements OnInit {
   count = 0;
   chart = [];
+  mChart = [];
 
   constructor(private analyticsService: AnalyticsService) { }
 
@@ -19,15 +20,19 @@ export class AnalyticsComponent implements OnInit {
     });
 
     this.analyticsService.getWeeklyCount().subscribe(data => {
-      var countData = data.json();
-      var label = [];
+      var countData = data.json(),
+          label = [],
+          mLabel = [];
 
       countData.date = countData.date.reverse();
+      countData.monthStr = countData.monthStr.reverse();
       for(var i = 0; i < countData.date.length; i++) {
         var dt = new Date(countData.date[i]);
         label.push(dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate());
+        mLabel.push(countData.monthStr[i]);
       }
       label.push('이번 주');
+      mLabel.push('이번 달');
 
       this.chart = new Chart('canvas', 
       {
@@ -43,7 +48,26 @@ export class AnalyticsComponent implements OnInit {
         options: {
           title: {
             display: true,
-            text: '주 별 생성된 번호 셋 개수'
+            text: '주별 생성된 번호 셋 개수'
+          }
+        }
+      });
+
+      this.mChart = new Chart('mCanvas', 
+      {
+        type: 'bar',
+        data: {
+          labels: mLabel,
+          datasets: [{
+            label: '번호 셋 개수',
+            data: countData.monthCounts.reverse(),
+            borderWidth: 1
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: '월별 생성된 번호 셋 개수'
           }
         }
       });
